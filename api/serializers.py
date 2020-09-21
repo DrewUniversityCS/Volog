d887a88b9d2c36628c55045d1f59601c071c7976
NullDefault <davisha999@gmail.com>
2020-09-21 12:08:28 -0700

    added React

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from api.models import Student
 
 
-class StudentSerializer(serializers.HyperlinkedModelSerializer):
+class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('name', 'student_id')
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,10 @@
 from django.urls import include, path
-from rest_framework import routers
 from api import views
 
-router = routers.DefaultRouter()
-router.register(r'students', views.HeroViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/lead/', views.HeroViewSet.as_view()),
 ]
