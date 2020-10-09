751c22f17fef84ef2d6d6201cb57277b3d19ace6
NullDefault <davisha999@gmail.com>
2020-10-09 14:41:07 -0700

    small typo

--- api/logistics/serializers.py
+++ api/logistics/serializers.py
@@ -7,4 +7,3 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('first_name', 'last_name', 'student_id', 'email', 'class_standing')
-    
