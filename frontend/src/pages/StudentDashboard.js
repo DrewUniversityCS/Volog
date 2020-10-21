63d50dc19dd43a2fc28898fb0803a58d508e30be
perrfection <peterkinp123@gmail.com>
2020-10-21 01:53:26 -0400

    it workkkkkssss

--- frontend/src/pages/StudentDashboard.js
+++ frontend/src/pages/StudentDashboard.js
@@ -3,14 +3,13 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import VProgressBar from '../components/elements/ProgressBar'
 import Container from 'react-bootstrap/Container';
 // import Notification from "../components/elements/Notification";
-import ReportHours from "../components/input/ReportHours";
 import UserPic from "../components/elements/Cards/userPic";
 import "../../pageDesignspecs/studentPg.css"
-// import Row from "react-bootstrap";
-// import Col from "react-bootstrap";
+import ReportHours from "../components/input/ReportHours";
 
 class StudentDashboard extends React.Component {
     render() {
+
         return <Container>
             <Jumbotron className="jumbotron2">
                 <h1>Welcome Back!</h1>
@@ -27,7 +26,7 @@ class StudentDashboard extends React.Component {
                 </p>
                 <p>
                     <Container>
-                        <ReportHours buttonLabel="Report Hours" addItemToState={this.addItemToState}/>
+                        <ReportHours/>
                     </Container>
                 </p>
 
@@ -36,6 +35,7 @@ class StudentDashboard extends React.Component {
                 {/*    <Notification/>*/}
                 {/*</p>*/}
             </Jumbotron>
+
 
         </Container>
     }
