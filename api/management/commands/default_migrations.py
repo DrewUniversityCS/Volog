fb66f755d069cc0d8e190f7a3b3ce20b071bcbb2
NullDefault <davisha999@gmail.com>
2020-10-22 14:16:39 -0700

    updated test

--- api/management/commands/default_migrations.py
+++ api/management/commands/default_migrations.py
@@ -7,10 +7,13 @@ class Command(BaseCommand):
     This will make a bunch of migrations at once. Makes ya type less - good stuff.
     """
 
+    apps_to_migrate = ['api', 'user']
+
     def handle(self, *args, **options):
         self._load_data()
 
     def _load_data(self):
-        management.call_command('makemigrations', 'api')
-        management.call_command('makemigrations', 'user')
+        for app in self.apps_to_migrate:
+            management.call_command('makemigrations', app)
+
         management.call_command('migrate')
