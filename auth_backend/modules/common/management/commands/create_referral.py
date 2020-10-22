3c1ffc52a30f7d7824eecb83fe74a41a64c579a9
NullDefault <davisha999@gmail.com>
2020-10-22 14:22:17 -0700

    updated commands

---
+++ auth_backend/modules/common/management/commands/create_referral.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+from auth_backend.modules.user.models import Referral
+
+
+class Command(BaseCommand):
+    """
+    Creates and loads a referral code.
+    """
+    help = "Creates a referral code."
+
+    def handle(self, *args, **options):
+        self._add_referral(options['role'][0])
+
+    def add_arguments(self, parser):
+        parser.add_argument('role', nargs='+', type=int)
+
+    def _add_referral(self, role_p):
+        r = Referral.objects.create(role=role_p)
+        r.save()
+        print(r)
