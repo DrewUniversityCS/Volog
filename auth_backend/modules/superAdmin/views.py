400a8f572ab2bc1eb330c38701606d67846517c6
NullDefault <davisha999@gmail.com>
2020-10-30 13:19:34 -0700

    backend refactors (mostly regarding views)

--- auth_backend/modules/superAdmin/views.py
+++ auth_backend/modules/superAdmin/views.py
@@ -4,25 +4,17 @@ Purpose: Django views for rendering a variety of data.
 Comments:
 """
 
-from django.shortcuts import redirect, reverse
 from django.views.generic.edit import FormView
-from pandas import read_csv
-from rest_framework import (
-    viewsets as rest_viewsets,
-    filters as rest_filters,
-    permissions as rest_permissions,
-    pagination as rest_pagination
-)
 
-from auth_backend.modules.common import constants as common_constants
 from auth_backend.modules.common.mixins import LoginRequiredMixin, AdminRequiredMixin
-from auth_backend.modules.superAdmin.admin_permissions import AdminRequired
 from auth_backend.modules.superAdmin.forms import ReferralCreateForm
-from auth_backend.modules.user.models import Referral, BaseVologUser
-from auth_backend.modules.user.serializers import UserSerializer
+from auth_backend.modules.user.models import Referral
 
 
 class CreateReferralView(LoginRequiredMixin, AdminRequiredMixin, FormView):
+    """
+    Django view for creating platform referral codes.
+    """
     template_name = 'superAdmin/referral_create.html'
     form_class = ReferralCreateForm
     success_url = '/superAdmin/referrals'
@@ -37,50 +29,3 @@ class CreateReferralView(LoginRequiredMixin, AdminRequiredMixin, FormView):
             'referrals': Referral.objects.order_by('-created_at')
         })
         return context
-
-
-class UserView(rest_viewsets.ModelViewSet):
-    """
-    User list and detail view
-    """
-
-    serializer_class = UserSerializer
-    permission_classes = (
-        rest_permissions.IsAuthenticated, AdminRequired,
-    )
-    filter_backends = [
-        rest_filters.OrderingFilter,
-        rest_filters.SearchFilter
-    ]
-    search_fields = ('email', 'first_name')
-    pagination_class = rest_pagination.PageNumberPagination
-    ordering = ('-created_at',)
-
-    def get_queryset(self):
-        role = self.request.GET.get('role')
-        query = BaseVologUser.objects.filter(is_profile_complete=True)
-        if role == 'student':
-            query = query.filter(role=common_constants.ROLE.STUDENT)
-        elif role == 'Admin':
-            query = query.filter(role=common_constants.ROLE.ADMIN)
-        elif role == 'mentor':
-            query = query.filter(role=common_constants.ROLE.TEACHER)
-        return query
-
-
-def bulk_invite(request):
-    if request.method == 'POST':
-        file = request.FILES['invites']
-        reader = read_csv(file)
-        for ind, value in reader.iterrows():
-            email = value['email']
-            role = value['role']
-            if role == 'faculty':
-                user_role = common_constants.ROLE.FACULTY
-            elif role == 'student':
-                user_role = common_constants.ROLE.STUDENT
-            else:
-                user_role = common_constants.ROLE.MENTOR
-            Referral.objects.create(email=email, role=user_role)
-
-    return redirect(reverse('superAdmin:referral'))
--- auth_backend/modules/user/views.py
+++ auth_backend/modules/user/views.py
@@ -6,12 +6,13 @@ Comments:
 from django.urls import reverse
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
-from auth_backend.modules.common.mixins import LoginRequiredMixin
-from auth_backend.modules.user.forms import ProfileForm
-from rest_framework.views import APIView
 from rest_framework import permissions
 from rest_framework.response import Response
-from .serializers import UserSerializer
+from rest_framework.views import APIView
+
+from auth_backend.modules.common.mixins import LoginRequiredMixin
+from auth_backend.modules.user.forms import ProfileForm
+from auth_backend.modules.user.serializers import UserSerializer
 
 
 class ProfileCreationView(FormView):
@@ -37,12 +38,15 @@ class ProfileCreationView(FormView):
 
 
 class ProfileSuccessfullyCreatedView(LoginRequiredMixin, TemplateView):
+    """
+    Django view to redirect users to after they have successfully created their profile.
+    """
     template_name = "user/profile_create_success.html"
 
 
-class UserApiView(APIView):
+class GetRequestUserData(APIView):
     """
-    API endpoint to retrieve user info
+    API view to get a JSON representation of the current (session) user
     """
     permission_classes = (permissions.IsAuthenticated,)
 
