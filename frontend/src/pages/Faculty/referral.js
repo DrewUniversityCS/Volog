f827afb7cd9880656cc9794289a7b2bc6a930990
malminawi <mahmoud@alminawi.org>
2020-11-01 09:15:31 -0500

    referrals issue fixed

--- frontend/src/pages/Faculty/referral.js
+++ frontend/src/pages/Faculty/referral.js
@@ -1,42 +1,12 @@
 import React, {Component} from 'react';
 
 class Referral extends Component {
-    state = {
-        html: "",
-        isLoading: true
-    };
-
-    componentDidMount() {
-        this.refreshIframe();
-
-        window.addEventListener('message', this.handleIframeTask);
-    }
-
-    refreshIframe = () => {
-        fetch("/superAdmin/referrals/", {method: 'GET'})
-            .then(response => response.text())
-            .then(res => {
-                this.setState({html: res, isLoading: false})
-            })
-    };
-
-    handleIframeTask = (e) => {
-        this.setState({
-            html: "",
-            isLoading: true
-        }, () => {
-            this.refreshIframe();
-        })
-
-    };
-
     render() {
-        if (this.state.isLoading) <h1>Loading....</h1>
         return (
-            <iframe width={"100%"} srcDoc={this.state.html}/>
+            <iframe width={"100%"} src='/superAdmin/referrals'/>
         )
-
     }
 }
+
 
 export default Referral;
