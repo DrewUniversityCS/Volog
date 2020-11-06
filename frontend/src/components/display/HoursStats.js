31f4ced75bc6a64541d99168b709712c04800b7b
NullDefault <davisha999@gmail.com>
2020-11-06 14:17:24 -0500

    graph tab pagination

--- frontend/src/components/display/HoursStats.js
+++ frontend/src/components/display/HoursStats.js
@@ -1,11 +1,39 @@
 import React from "react";
+import {MDBContainer} from "mdbreact";
+import {Col, Row, Tab, Tabs} from "react-bootstrap";
 
 class HoursStats extends React.Component {
+    state = {
+        active_graph: 'category-summary'
+    }
+
     render() {
+        let active_graph;
+        if (this.state.active_graph === 'category-summary') {
+            active_graph = <div>CATEGORIES</div>
+        } else {
+            active_graph = <div>Whoa there</div>
+        }
+
+
         return (
-            <div>
-                Test
-            </div>
+            <MDBContainer>
+                <Row>
+                    <Col sm={3}>
+                        <Tabs variant="pills"
+                              className="flex-column"
+                              defaultActiveKey="category-summary"
+                              activeKey={this.state.active_graph}
+                              onSelect={(k) => this.setState({active_graph: k})}>
+                            <Tab eventKey="category-summary" title="By Categories"/>
+                            <Tab eventKey="time-series" title="Over Time"/>
+                        </Tabs>
+                    </Col>
+                    <Col sm={3}>
+                        {active_graph}
+                    </Col>
+                </Row>
+            </MDBContainer>
         )
     }
 }
