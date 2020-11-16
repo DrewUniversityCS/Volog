b422cbb9bb4337ecd37cf0c31cd56ece7a7ba03d
malminawi <mahmoud@alminawi.org>
2020-11-16 15:30:02 -0500

    fixing bug

--- api/settings/urls.py
+++ api/settings/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     path('api/activity_categories/', api.views.hour_views.ActivityCategoriesView.as_view()),
     path('api/group/students/current', api.views.mentor_views.GroupStudentsListView.as_view()),
     path('api/report/student/hours', api.views.hour_views.student_hour_report),
+    path('api/report/hours', api.views.hour_views.hour_report)
+
 ]
 
 urlpatterns += router.urls
