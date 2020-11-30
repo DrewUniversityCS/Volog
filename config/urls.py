009d142500b693585c5069d11bf68401e13d26ff
NullDefault <davisha999@gmail.com>
2020-11-30 12:11:17 -0500

    refactored csv functionality

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
-from django.urls import reverse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
