9cd06d00046f09e48009298c77c6b1f1bc1384a4
NullDefault <davisha999@gmail.com>
2020-12-02 11:11:37 -0500

    added whitenoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,6 +22,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'src', 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #####~DJANGO APPS~######################################################################################################
 INSTALLED_APPS = [
     # Django Apps
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 ####~MIDDLEWARE~########################################################################################################
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
