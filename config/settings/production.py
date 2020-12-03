35a01b6df565bfa3d4286dcf2510cd77dc6031a3
NullDefault <davisha999@gmail.com>
2020-12-03 15:11:09 -0500

    typo

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 #####~EMAIL SETTINGS~###################################################################################################
-EMAIL_BACKEND = "sendgrid_backend.SendGridBackend"
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 SENDGRID_API_KEY = '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'
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 SENDGRID_ECHO_TO_STDOUT = True
