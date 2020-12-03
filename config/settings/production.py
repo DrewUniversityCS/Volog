b0160ca8075c33d2d3947fe364a091c9e65f78ce
NullDefault <davisha999@gmail.com>
2020-12-03 14:38:25 -0500

    fixin sum stuff

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,12 +1,10 @@
 from .base import *
 
 #####~EMAIL SETTINGS~###################################################################################################
-SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = "sgbackend.SendGridBackend"
+SENDGRID_API_KEY = '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'
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+SENDGRID_ECHO_TO_STDOUT = True
 ########################################################################################################################
 DEBUG = False
 X_FRAME_OPTIONS = 'ALLOW-FROM volog-test.herokuapp.com'
